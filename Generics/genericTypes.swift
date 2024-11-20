40c92c6d167b8966e03b19b6b6e5c4c82c84b997
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2024-11-20 11:02:35 +0330

    Create genericTypes.swift
    You can also use generics to define reusable types like a stack:
    • Element is the placeholder type for the stack.
    • The stack works with any type of data, such as Int, String, or a custom type.

---
+++ Generics/genericTypes.swift
@@ -0,0 +1,16 @@
+struct Stack<Element> {
+    private var items: [Element] = []
+
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+
+    mutating func pop() -> Element? {
+        return items.popLast()
+    }
+}
+
+var intStack = Stack<Int>()
+intStack.push(1)
+intStack.push(2)
+print(intStack.pop() ?? "Empty") // 2
